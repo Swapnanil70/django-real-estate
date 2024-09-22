29ea1c6cfeccb22f57d613af24e7332e637eca4c
Swapnanil <goswamibabin7@gmail.com>
2024-09-22 17:27:35 +0530

    added rating model and serializers.

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'pkid', 'user', 'gender', 'phone_number', 'city', 'country')
+    list_display = ['id', 'pkid', 'user', 'gender', 'phone_number', 'city', 'country']
     list_filter = ['gender', 'city', 'country']
     list_display_links = ['id', 'pkid', 'user']
     
