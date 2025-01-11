13b37474d61cfb289f33b776a116a7fd54c7157e
Swapnanil <goswamibabin7@gmail.com>
2025-01-11 23:11:00 +0530

    feat : Not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import PropertiesPage from './pages/PropertiesPage';
+import NotFound from './components/NotFound';
 
 const App = () => {
   return (
@@ -15,11 +17,12 @@ const App = () => {
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/properties' element={<PropertiesPage />} />
+            <Route path='/*' element={<NotFound />} key='404'/>
           </Routes>
+          <ToastContainer theme='dark' />
         </main>
         <Footer />
       </Router>
-      <ToastContainer />
     </>
   );
 };
